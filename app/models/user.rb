7f61f60bd2436cb969c5672c77e049cba95dc1fe
matt <vigneronmatthieu@hotmail.com>
2018-04-12 10:43:00 +0100

    add user method that delete and add new twitter followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-	has_many :followers
+	has_many :followers, :dependent => :destroy
 
 	def self.find_or_create_from_auth_hash(auth_hash)
 		user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
@@ -11,5 +11,28 @@ class User < ApplicationRecord
 			token: auth_hash.credentials.token
 		)
 		user
+	end
+
+	def get_followers_from_provider
+		case provider
+		when "twitter"
+			provider_followers = TwitterClient.new(self).followers
+		end
+		followers.delete_all
+		add_followers(provider_followers)
+	end
+
+	def add_followers provider_followers
+		new_list_of_followers = []
+		provider_followers.map do |provider_follower|
+			new_list_of_followers << {
+				user_id: id,
+				provider: provider, 
+				name: provider_follower.screen_name,
+				avatar: provider_follower.profile_image_url_https.to_s
+			}
+		end
+		# bulk insert for accounts with many followers
+		Follower.bulk_insert values: new_list_of_followers
 	end
 end
---
+++ spec/services/fake_twitter_client.rb
@@ -0,0 +1,15 @@
+class FakeTwitterClient
+
+	def initialize user
+		@client = Twitter::REST::Client.new do |config|
+			config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+			config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+			config.access_token        = '1234'
+			config.access_token_secret = 'secret'
+		end
+	end
+
+	def followers
+		[Twitter::User.new(id: '123456', screen_name: 'momo', profile_image_url_https: 'thatsmyavatar.com/avatar.png')]
+	end
+end 
