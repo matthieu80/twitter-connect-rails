c20ce666ace818827d707807263a40f0df5a7d66
matt <vigneronmatthieu@hotmail.com>
2018-04-13 07:03:20 +0100

    improve Twitter client specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,62 @@ RSpec.describe User, type: :model do
 			expect(user.followers.first.name).to eq "momo"
 		end
 	end
-end 
+
+
+	describe "Twitter client reach limit when fetching 'followers/ids'" do
+
+		before do
+			stub_const("TwitterClient", FakeTwitterClient)
+			allow_any_instance_of(FakeTwitterClient).to receive(:get_follower_ids_from_client).and_raise(Twitter::Error::TooManyRequests)
+		end
+
+		it 'should get rescued' do
+			user = create :user
+			user.get_followers_from_provider
+		end
+
+		it 'should not get previous followers deleted' do
+			
+			user = create :user
+			follower = create :follower
+
+			expect(user.followers.count).to eq 1
+			expect(user.followers.first.name).to eq follower.name
+
+			user.get_followers_from_provider
+
+			user.reload
+			expect(user.followers.count).to eq 1
+			expect(user.followers.first.name).to eq follower.name
+		end
+
+	end
+
+	describe "Twitter client reach limit when fetching '/lookup'" do
+
+		before do
+			stub_const("TwitterClient", FakeTwitterClient)
+			allow_any_instance_of(FakeTwitterClient).to receive(:get_users_from_client).and_raise(Twitter::Error::TooManyRequests)
+		end
+
+		it 'should get rescued' do
+			user = create :user
+			user.get_followers_from_provider
+		end
+
+		it 'should not get previous followers deleted' do
+			
+			user = create :user
+			follower = create :follower
+
+			expect(user.followers.count).to eq 1
+			expect(user.followers.first.name).to eq follower.name
+
+			user.get_followers_from_provider
+
+			user.reload
+			expect(user.followers.count).to eq 1
+			expect(user.followers.first.name).to eq follower.name
+		end
+	end
+end
--- spec/services/fake_twitter_client.rb
+++ spec/services/fake_twitter_client.rb
@@ -1,6 +1,7 @@
 class FakeTwitterClient
 
 	def initialize user
+		@user = user
 		@client = Twitter::REST::Client.new do |config|
 			config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
 			config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
@@ -10,6 +11,38 @@ class FakeTwitterClient
 	end
 
 	def get_followers
+		collection = []
+		ids = get_follower_ids
+		return [] if !ids
+		for i in 0..ids.count / 100
+			section = ids.slice(i*100, (i+1)*100)
+			users = lookup(section)
+			collection << users unless !users
+		end
+		collection.flatten
+	end
+
+	def get_follower_ids
+		begin
+			get_follower_ids_from_client
+		rescue Twitter::Error::TooManyRequests => e
+			nil
+		end
+	end
+
+	def lookup user_ids
+		begin
+			get_users_from_client
+		rescue Twitter::Error::TooManyRequests => e
+			nil
+		end
+	end
+
+	def get_follower_ids_from_client
+		['1']
+	end
+
+	def get_users_from_client
 		[Twitter::User.new(id: '123456', screen_name: 'momo', profile_image_url_https: 'thatsmyavatar.com/avatar.png')]
 	end
-end 
+end
